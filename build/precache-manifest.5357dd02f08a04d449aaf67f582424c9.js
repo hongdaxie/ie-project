4dd014270266eef17bda70d22f8487d53dfcc816
hongda <1993xiehongda@gmail.com>
2019-09-15 22:46:11 +1000

    add build folder

---
+++ build/precache-manifest.5357dd02f08a04d449aaf67f582424c9.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07f4151e2bc1c49937ff0b1541c905ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba0814b7c3bb280b20f3",
+    "url": "/static/css/0.7306d69c.chunk.css"
+  },
+  {
+    "revision": "7c805077d486a923b10b",
+    "url": "/static/css/1.eb0f9185.chunk.css"
+  },
+  {
+    "revision": "ce93c010cc6fb80fae26",
+    "url": "/static/css/4.439b4ef8.chunk.css"
+  },
+  {
+    "revision": "7f0c3f29fb24df6a77dc",
+    "url": "/static/css/5.95817749.chunk.css"
+  },
+  {
+    "revision": "acc3e82bf579e8304157",
+    "url": "/static/css/6.d6c0085b.chunk.css"
+  },
+  {
+    "revision": "d0b601a74bfce374e601",
+    "url": "/static/css/7.82a0ab1d.chunk.css"
+  },
+  {
+    "revision": "6e0a2bf73f280f991dc0",
+    "url": "/static/css/9.09a33ab8.chunk.css"
+  },
+  {
+    "revision": "e612c52a6c3e765ae80a",
+    "url": "/static/css/main.a5aa01db.chunk.css"
+  },
+  {
+    "revision": "ba0814b7c3bb280b20f3",
+    "url": "/static/js/0.04a03217.chunk.js"
+  },
+  {
+    "revision": "7c805077d486a923b10b",
+    "url": "/static/js/1.3c79adf2.chunk.js"
+  },
+  {
+    "revision": "6806ced40c213a91e3c7",
+    "url": "/static/js/10.158997a0.chunk.js"
+  },
+  {
+    "revision": "3884fc98087cf7b1a454",
+    "url": "/static/js/11.f643bf3c.chunk.js"
+  },
+  {
+    "revision": "a978c85914c3aa1a5f94",
+    "url": "/static/js/12.0a2161a2.chunk.js"
+  },
+  {
+    "revision": "312ce85307f096122239",
+    "url": "/static/js/13.aadaf46f.chunk.js"
+  },
+  {
+    "revision": "ce93c010cc6fb80fae26",
+    "url": "/static/js/4.de3f131f.chunk.js"
+  },
+  {
+    "revision": "7f0c3f29fb24df6a77dc",
+    "url": "/static/js/5.30d431fe.chunk.js"
+  },
+  {
+    "revision": "acc3e82bf579e8304157",
+    "url": "/static/js/6.44ad987e.chunk.js"
+  },
+  {
+    "revision": "d0b601a74bfce374e601",
+    "url": "/static/js/7.e82d650e.chunk.js"
+  },
+  {
+    "revision": "720e4dfafc5f36adfeb4",
+    "url": "/static/js/8.39b9d63c.chunk.js"
+  },
+  {
+    "revision": "6e0a2bf73f280f991dc0",
+    "url": "/static/js/9.b1adb162.chunk.js"
+  },
+  {
+    "revision": "e612c52a6c3e765ae80a",
+    "url": "/static/js/main.f1fab7e5.chunk.js"
+  },
+  {
+    "revision": "950e001d57319f259b5a",
+    "url": "/static/js/runtime~main.c5887d82.js"
+  },
+  {
+    "revision": "8fe6ab9a8babdedce3955e209467c1cb",
+    "url": "/static/media/banner1bg.8fe6ab9a.jpg"
+  },
+  {
+    "revision": "8b4d4fb4f0fe16463e40c99c8a7bd614",
+    "url": "/static/media/banner2bg.8b4d4fb4.jpg"
+  },
+  {
+    "revision": "1c5831a7a0c88391235905ab1d0b1d78",
+    "url": "/static/media/banner3bg.1c5831a7.jpg"
+  },
+  {
+    "revision": "af9b5339169d1085dacadd27f80cdb26",
+    "url": "/static/media/homePageRightSide1.af9b5339.jpg"
+  },
+  {
+    "revision": "c0db7fd5408201fe0ecb2ccbca50d585",
+    "url": "/static/media/homePageRightSide1.c0db7fd5.jpg"
+  },
+  {
+    "revision": "2a2d160e49a8365099a1ef2729ff636a",
+    "url": "/static/media/homePageRightSide2.2a2d160e.jpg"
+  },
+  {
+    "revision": "bf34923a4039fe537afc89b8ca1d8fbe",
+    "url": "/static/media/homePageRightSide2.bf34923a.jpg"
+  },
+  {
+    "revision": "ea8628069a8405c2393dc0116beb5cb3",
+    "url": "/static/media/homePageRightSide3.ea862806.jpg"
+  },
+  {
+    "revision": "583a11a53da62baab73c0748913873c8",
+    "url": "/static/media/homePageRightSide4.583a11a5.jpg"
+  },
+  {
+    "revision": "18fa4f235ec480f0e094d6e94af1bf8d",
+    "url": "/static/media/logo.18fa4f23.png"
+  },
+  {
+    "revision": "c9b3392d980860e42ff23ef8e38b98c5",
+    "url": "/static/media/teamMember1.c9b3392d.jpeg"
+  },
+  {
+    "revision": "1aad0a71c6e2331d60cb4a2ddcc8f1ae",
+    "url": "/static/media/teamMember2.1aad0a71.jpeg"
+  },
+  {
+    "revision": "c2221b2feda2f94c8bc7f1d09068821e",
+    "url": "/static/media/teamMember3.c2221b2f.jpeg"
+  },
+  {
+    "revision": "70b781f3a669ec8214e6c724892239b8",
+    "url": "/static/media/teamMember4.70b781f3.jpeg"
+  },
+  {
+    "revision": "0eb199a39ac965213d570b4b2f378d7b",
+    "url": "/static/media/vaccine1.0eb199a3.jpg"
+  },
+  {
+    "revision": "d26e2b43fd46b195d4abfdf5ecafd9dd",
+    "url": "/static/media/vaccine2.d26e2b43.jpg"
+  }
+]);
